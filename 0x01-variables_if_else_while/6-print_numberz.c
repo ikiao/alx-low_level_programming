711639d497d9efd14e5b8c50c77c21efd07d6d4a
ikiao <mwitidouglas31@gmail.com>
2023-03-17 10:23:01 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - print numbers
+ *for - loop in use
+ *putchar - to convert to ASCII value
+ *Return: zero
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	putchar((n % 10) + '0');
+	putchar('\n');
+	return (0);
+}
+Footer
+© 2023 GitHub, Inc.
